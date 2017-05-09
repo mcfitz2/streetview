d73d1d9a6d05c3cde07967bd7fa0989f7bd887bd
Micah Fitzgerald <mcfitz2@gmail.com>
2017-05-09 20:09:32 +0000

    removed redundant promise code. added metadata method. added module.exports so you can actually use it

--- index.js
+++ index.js
@@ -3,17 +3,17 @@ var request = require("request-promise-native");
 function streetview(opts) {
     this.key = opts.api_key;
 }
-streetview.prototype.get = function(opts) {
-	if (!opts.location && !opts.pano && !opts.lat && !opts.lng) {
-		return Promse.reject("You must supply a location, lat/lng, or panorama ID");
-	}
-	if ((opts.lat && !opts.lng) || (opts.lng && !opts.lat)) {
-		return Promise.reject("You must supply both lat and lng");
-	}
-	if (!opts.size || opts.size.length != 2) {
-		return Promise.reject("Missing or invalid size parameter");
-	}
-	var BASE = "https://maps.googleapis.com/maps/api/streetview"
+streetview.prototype.image = function(opts) {
+    if (!opts.location && !opts.pano && !opts.lat && !opts.lng) {
+        return Promse.reject("You must supply a location, lat/lng, or panorama ID");
+    }
+    if ((opts.lat && !opts.lng) || (opts.lng && !opts.lat)) {
+        return Promise.reject("You must supply both lat and lng");
+    }
+    if (!opts.size || opts.size.length != 2) {
+        return Promise.reject("Missing or invalid size parameter");
+    }
+    var BASE = "https://maps.googleapis.com/maps/api/streetview"
     opts.key = this.key;
     if (opts.location === null) {
         opts.location = opts.lat + "," + opts.lng;
@@ -21,15 +21,33 @@ streetview.prototype.get = function(opts) {
     delete opts.lat;
     delete opts.lng;
     opts.size = opts.size[0] + "x" + opts.size[1];
-    console.log(opts);
     return request({
         url: BASE,
         qs: opts,
         method: "GET",
         encoding: null
-    }).then((body) => {
-        return Promise.resolve(body);
-    }).catch((err) => {
-        return Promise.reject(err);
     });
 }
+
+streetview.prototype.metadata = function(opts) {
+    if (!opts.location && !opts.pano && !opts.lat && !opts.lng) {
+        return Promse.reject("You must supply a location, lat/lng, or panorama ID");
+    }
+    if ((opts.lat && !opts.lng) || (opts.lng && !opts.lat)) {
+        return Promise.reject("You must supply both lat and lng");
+    }
+    var BASE = "https://maps.googleapis.com/maps/api/streetview/metadata"
+    opts.key = this.key;
+    if (opts.location === null) {
+        opts.location = opts.lat + "," + opts.lng;
+    }
+    delete opts.lat;
+    delete opts.lng;
+    return request({
+        url: BASE,
+        qs: opts,
+        method: "GET",
+        json: true
+    });
+}
+module.exports = streetview;
